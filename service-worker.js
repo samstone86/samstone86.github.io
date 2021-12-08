838f905c0e765bbabdcabf36fe3cc4830a033caf
Philipp Vogel <philippwolf86.pw@gmail.com>
2021-12-08 19:40:09 +0100

    deploy 081221

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
 importScripts("/precache-manifest.3180c73eb90ca6a4b40aeef0690bfd16.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-// define a prefix for your cache names. It is recommended to use your project name
-//workbox.core.setCacheNameDetails({prefix:  "galle-pwa"});
-
 workbox.setConfig({
     debug: true
 });
+
+// define a prefix for your cache names. It is recommended to use your project name
+workbox.core.setCacheNameDetails({prefix:  "galle-pwa"});
 
 // These lines are responsible for precaching static resources. Using 'generateSW'(zero config), these lines would be added automatically to the serviceworker.
 // __precacheManifest is the list of resources you want to precache. It will be generated and imported automatically by workbox during build time
@@ -15,7 +15,7 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerRoute(
     new RegExp("https://fonts.(?:googleapis|static).com/(.*)"),
-    workbox.strategies.CacheFirst({
+    new workbox.strategies.CacheFirst({
         cacheName: "googleapis",
         plugins: [
             new workbox.expiration.Plugin({
@@ -26,23 +26,5 @@ workbox.routing.registerRoute(
         cacheableResponse: { statuses: [0, 200]}
     })
 );
-
-
-
-
-// Here is where we apply our customization for the api
-// all the api request wich starts with this pattern(first parameter) will use CacheFirst strategy for caching
-/*
-workbox.routing.registerRoute(
-    /https:\/\/erp\.galle-gmbh\.de\/pwa\//,
-    new  workbox.strategies.CacheFirst()
-);
-
-workbox.routing.registerRoute(
-    /https:\/\/testerp\.galle-gmbh\.de\/pwa\//,
-    new  workbox.strategies.CacheFirst()
-);
-*/
-
 
 // @source https://www.youtube.com/watch?v=15Yr-J4X34M&t=1107s
