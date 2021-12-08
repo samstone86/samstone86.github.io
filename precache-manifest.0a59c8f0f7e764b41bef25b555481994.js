4a918645f250c61ff12d276aa4258c546dd343d6
Philipp Vogel <philippwolf86.pw@gmail.com>
2021-12-08 17:11:17 +0100

    Add files via upload

---
+++ precache-manifest.0a59c8f0f7e764b41bef25b555481994.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd93b6fadb008a3db49e",
+    "url": "/css/app.91227d19.css"
+  },
+  {
+    "revision": "d08de6ad72814007a1bf",
+    "url": "/css/chunk-vendors.6e1694df.css"
+  },
+  {
+    "revision": "478dbf4e5a762fc013c2",
+    "url": "/css/time-tracking.27798df6.css"
+  },
+  {
+    "revision": "f2d743c16504176cbf89502dbedc7b9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd93b6fadb008a3db49e",
+    "url": "/js/app.9051d67c.js"
+  },
+  {
+    "revision": "d08de6ad72814007a1bf",
+    "url": "/js/chunk-vendors.f02762c5.js"
+  },
+  {
+    "revision": "478dbf4e5a762fc013c2",
+    "url": "/js/time-tracking.af712dd4.js"
+  },
+  {
+    "revision": "29498d3ad2ce174dec17aba477fd6146",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,48 @@
-importScripts("/precache-manifest.d8ff3f5d0963227437c79b1bb54b923b.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.0a59c8f0f7e764b41bef25b555481994.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 // define a prefix for your cache names. It is recommended to use your project name
-workbox.core.setCacheNameDetails({prefix:  "galle-pwa"});
+//workbox.core.setCacheNameDetails({prefix:  "galle-pwa"});
+
+workbox.setConfig({
+    debug: true
+});
 
 // These lines are responsible for precaching static resources. Using 'generateSW'(zero config), these lines would be added automatically to the serviceworker.
 // __precacheManifest is the list of resources you want to precache. It will be generated and imported automatically by workbox during build time
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
+
+workbox.routing.registerRoute(
+    new RegExp("https://fonts.(?:googleapis|static).com/(.*)"),
+    workbox.strategies.CacheFirst(
+        cacheName: "googleapis",
+        plugins: [
+            new workbox.expiration.Plugin({
+                maxEntries: 30
+            })
+        ],
+        method: "GET",
+        cacheableResponse: { statuses: [{0, 200}]}
+    })
+);
+
+
+
+
 // Here is where we apply our customization for the api
 // all the api request wich starts with this pattern(first parameter) will use CacheFirst strategy for caching
+/*
 workbox.routing.registerRoute(
-    /https:\/\/get\.geojs\.io\/v1\/ip\/country\.json/,
+    /https:\/\/erp\.galle-gmbh\.de\/pwa\//,
     new  workbox.strategies.CacheFirst()
 );
 
 workbox.routing.registerRoute(
-    '/pwa/checkLogin',
-    async ({event}) => staleWhileRevalidate(event),
-    'POST'
+    /https:\/\/testerp\.galle-gmbh\.de\/pwa\//,
+    new  workbox.strategies.CacheFirst()
 );
+*/
 
-workbox.routing.registerRoute(
-    '/pwa/toggleTimeTrack',
-    async ({event}) => staleWhileRevalidate(event),
-    'POST'
-);
+
+// @source https://www.youtube.com/watch?v=15Yr-J4X34M&t=1107s
