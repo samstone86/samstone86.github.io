ae37b5ccaeba9a6195e23ce5f922852cf8bf463d
Philipp Vogel <philippwolf86.pw@gmail.com>
2021-12-08 08:32:00 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
-importScripts("/precache-manifest.7649abcd267e2cb998a48c156a5f85a7.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.e121507b6e25f7e99cd47a10f98a37ad.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 // define a prefix for your cache names. It is recommended to use your project name
-workbox.core.setCacheNameDetails({prefix:  "galle-pwa-project"});
+workbox.core.setCacheNameDetails({prefix:  "galle-pwa"});
 
 // These lines are responsible for precaching static resources. Using 'generateSW'(zero config), these lines would be added automatically to the serviceworker.
 // __precacheManifest is the list of resources you want to precache. It will be generated and imported automatically by workbox during build time
@@ -11,6 +11,12 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 // Here is where we apply our customization for the api
 // all the api request wich starts with this pattern(first parameter) will use CacheFirst strategy for caching
 workbox.routing.registerRoute(
-    /https:\/\/samstone86\.github\.io\//,
+    /https:\/\/get\.geojs\.io\/v1\/ip\/country\.json/,
     new  workbox.strategies.CacheFirst()
+);
+
+workbox.routing.registerRoute(
+    '/graphql',
+    async ({event}) => staleWhileRevalidate(event),
+    'POST'
 );
